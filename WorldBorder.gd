02b1bb82c0a135d22fb3a1175e9fd704f3922d83
Emily <tweetychrip@gmail.com>
2022-02-22 14:38:58 -0800

    Moved files around

--- WorldBorder.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-onready var area = $CollisionShape2D
-
-func onEntryHallwaybodyentered(body):
-gettree().changescene("res://World/room/scene2.tscn")
-#AaAAAAARG I DONT KNOW HOW TO MAKE IT SCROLL TO THE NEXT SCREEN BUT ALSO
-#FOLLOW THE CHARACTER WHEN THERE'S NO BORDER NEARBY AAAAAHHH
